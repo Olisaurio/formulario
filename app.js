310b32fb58dfa0006022fc978aa7af6278b78943
Olisaurio <santioro1736@gmail.com>
2024-08-03 11:25:29 -0500

    Add styles and modifed app.js

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-const form = document.getElementById('formIndex');
+/*const form = document.getElementById('formIndex');
 form.addEventListener('submit', sendEmail)
 
-const seviceId = 'service_kl3065j'
+const serviceId = 'service_kl3065j'
 const templateId = 'template_v98joke'
 const apiKey = '6LC59AszJuZqQ4oF9'
 
 
 function sendEmail(event) {
     event.preventDefault()
-    emailjs.init(seviceId)
+    emailjs.init(serviceId)
 
-    emailjs.sedForm(seviceId,templateId,form,apiKey)
+    emailjs.sedForm(serviceId,templateId,form,apiKey)
     .then(result => Swal.fire("su mensaje se ha enviado con exito"))
-    .catch(error => {
+    .catch((error) => {
         Swal.fire({
-            icon: 'error',
-            tittle: 'Oops...',
-            text: 'Hubo un error al enviar el mensaje, intentelo más tarde'
+            icon: "error",
+            tittle: "Oops...",
+            text: "Hubo un error al enviar el mensaje, intentelo más tarde"
         })
     })
+}*/
+
+const form = document.getElementById('formIndex');
+form.addEventListener('submit', sendEmail);
+
+const serviceId = 'service_kl3065j';
+const templateId = 'template_v98joke';
+const apiKey = '6LC59AszJuZqQ4oF9';
+
+function sendEmail(event) {
+    event.preventDefault();
+    
+    emailjs.init(apiKey);
+    
+    emailjs.sendForm(serviceId, templateId, form)
+        .then(result => {
+            Swal.fire("Su mensaje se ha enviado con éxito");
+        })
+        .catch((error) => {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Hubo un error al enviar el mensaje, inténtelo más tarde"
+            });
+        });
 }
